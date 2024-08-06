640b4c90a1e785e919219cbe4edc98a280984fe7
Nelson Baguma <nelsonbaguma@Nelsons-MacBook-Air.local>
2024-08-05 23:32:20 -0400

    added demo email and pass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import DisplayGrades from "@/components/DisplayGrades";
 import StudentForm from "@/components/StudentForm";
+import TestCredentials from "@/components/TestCredentials";
 
 export default function page() {
   return (
     <main className="flex min-h-screen flex-col gap-6 items-center justify-center p-24">
+      <TestCredentials />
       <StudentForm />
     </main>
   );
---
+++ src/components/TestCredentials.tsx
@@ -0,0 +1,18 @@
+import { Group } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+
+const TestCredentials = () => {
+  return (
+    <Card className="min-w-[350px]">
+      <CardHeader>
+        <CardTitle>Test Credentials</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>Email: gloria.peruth@cmsdk12.org</p>
+        <p>Password: G26097gg</p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default TestCredentials;
